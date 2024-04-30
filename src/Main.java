4e1e5ee829b1ee7bfcc3d7c9cbe2e06903229008
origin <maryna.oganesyan@yandex.ru>
2024-04-30 18:29:57 +0400

    HW

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author = new Author("Александр", "Пушкин");
+        Author author2 = new Author("Александр", "Пушкин");
+        Book book = new Book(author, "Капитанская дочка", 1836);
+        book.setPublishYear(1830);
+
+        System.out.println(book);
+        System.out.println(author.equals(author2));
+
+
     }
 }
